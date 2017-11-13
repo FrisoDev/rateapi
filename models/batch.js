e6f99b0eb7aae2e6b362976772b3078f4627bd07
FrisoDev <frisodevstoker@gmail.com>
2017-11-13 20:17:53 +0100

    tweak API models

--- models/batch.js
+++ models/batch.js
@@ -1,18 +1,26 @@
-// models/game.js
 const mongoose = require('../config/database')
 const { Schema } = mongoose
 
+const evalSchema = new Schema({
+  date: { type: Date, default: Date.now }
+  evaluation: { type: Number, default 0}
+  remark: { type: String}
+})
+
 const studentSchema = new Schema({
-  studentId: { type: Schema.Types.ObjectId, ref: 'students' },
+  name: { type: String, default: "Big Shaq"}
+  evaluations: [evalSchema]
+  image_url: { type: String, default: "https://i2.wp.com/www.kingsizemag.se/wp-content/uploads/2017/11/big-shaq-genius-mans-not-ls.jpg" }
+  userId: { type: Schema.Types.ObjectId, ref: 'users' },
 });
 
 const batchSchema = new Schema({
-  batchNumber: { type: Number, default: 0 },
   students: [studentSchema],
-  startDate: { type: Date, default: Date.now },
-  endDate: { type: Date, default: Date.now },
+  startDate: { type: Date, default: Date.now }
+  endDate: { type: Date, default: Date.now }
+  userId: { type: Schema.Types.ObjectId, ref: 'users' },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
-});
+}, {usePushEach:true})
 
 module.exports = mongoose.model('batches', batchSchema)
